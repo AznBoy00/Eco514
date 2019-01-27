67f39c2e8edd36fc910a2839d83c71507768e9a2
unknown <davebhardwaj@gmail.com>
2019-01-26 19:12:44 -0500

    added camera func

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,4 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class CameraApp extends StatefulWidget {
+  @override
+  _CameraAppState createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+        aspectRatio:
+        controller.value.aspectRatio,
+        child: CameraPreview(controller));
+  }
+}
+
+List<CameraDescription> cameras;
 
 void main() => runApp(MyApp());
 
@@ -108,4 +149,5 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }
